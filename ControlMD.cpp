2feab80cc81768b49674819065df333deb1d5e68
立脇拓 <takuji.coroton.thanks@gmail.com>
2020-09-08 01:33:09 +0000

    Update ControlMD.cpp

--- ControlMD.cpp
+++ ControlMD.cpp
@@ -16,19 +16,19 @@
  }
 
 void ControlMD::addMD(uint8_t number, TIM_HandleTypeDef *pTim, uint32_t pChannel, TIM_HandleTypeDef *nTim, uint32_t nChannel){
-  MD_t tmpArgument = {pTim , pChannel , nTim , nChannel , 0};
-  MD.insert(std::make_pair(number, tmpArgument));
+	MD_t tmpArgument = {pTim , pChannel , nTim , nChannel , 0};
+	MD.insert(std::make_pair(number, tmpArgument));
 }
 
 void ControlMD::init(uint8_t number, double maxSpeedPercent){
-  if(MD.find(number) == MD.end()){
-	  return;
-  }
-  if(MD[number].pTim->Instance->PSC >= MD[number].nTim->Instance->PSC){
-	  MD[number].maxCounterPeriod = (uint16_t)MD[number].nTim->Init.Period * maxSpeedPercent / 100;
-  }else{
-	  MD[number].maxCounterPeriod = (uint16_t)MD[number].pTim->Init.Period * maxSpeedPercent / 100;
-  }
+	if(MD.find(number) == MD.end()){
+		return;
+	}
+	if(MD[number].pTim->Instance->PSC >= MD[number].nTim->Instance->PSC){
+		MD[number].maxCounterPeriod = (uint16_t)MD[number].nTim->Init.Period * maxSpeedPercent / 100;
+	}else{
+		MD[number].maxCounterPeriod = (uint16_t)MD[number].pTim->Init.Period * maxSpeedPercent / 100;
+  	}
 }
 
 void ControlMD::start(uint8_t number){
@@ -62,9 +62,9 @@ uint8_t ControlMD::setSpeed(uint8_t number, int16_t speed){
 		return 1;
 	}
 	if(speed < -MD[number].maxCounterPeriod or MD[number].maxCounterPeriod < speed){
-    resetSpeed(number);
-     return 1;
-   }
+    	resetSpeed(number);
+    	return 1;
+	}
 	if(speed < 0){
 		__HAL_TIM_SET_COMPARE(MD[number].pTim, MD[number].pChannel, 0);
 		__HAL_TIM_SET_COMPARE(MD[number].nTim, MD[number].nChannel, -speed);
@@ -74,8 +74,8 @@ uint8_t ControlMD::setSpeed(uint8_t number, int16_t speed){
 	} else {
 		__HAL_TIM_SET_COMPARE(MD[number].pTim, MD[number].pChannel, 0);
 		__HAL_TIM_SET_COMPARE(MD[number].nTim, MD[number].nChannel, 0);
-	}
-  return 0;
+	}	
+	return 0;
 }
 
 void ControlMD::setSpeedPercent(uint8_t number , int8_t percent){
