cfa24f9843edd34b510eb0561b744aa99f04a9a5
SatoshiRC <44540173+SatoshiRC@users.noreply.github.com>
2023-02-09 14:26:37 +0900

    change:管理するMDを1個にした

--- ControlMD.cpp
+++ ControlMD.cpp
@@ -7,80 +7,49 @@
 
  #include"ControlMD.hpp"
 
- ControlMD::ControlMD(){
-
- }
-
- ControlMD::~ControlMD(){
-	 MD.clear();
- }
-
-void ControlMD::addMD(uint8_t number, TIM_HandleTypeDef *pTim, uint32_t pChannel, TIM_HandleTypeDef *nTim, uint32_t nChannel){
-	MD_t tmpArgument = {pTim , pChannel , nTim , nChannel , 0};
-	MD.insert(std::make_pair(number, tmpArgument));
-}
-
-void ControlMD::init(uint8_t number, double maxSpeedPercent){
-	if(MD.find(number) == MD.end()){
-		return;
-	}
-	if(MD[number].pTim->Instance->PSC >= MD[number].nTim->Instance->PSC){
-		MD[number].maxCounterPeriod = (uint16_t)MD[number].nTim->Init.Period * maxSpeedPercent / 100;
+void ControlMD::init(float maxSpeedPercent){
+	if(pTim->Instance->PSC >= nTim->Instance->PSC){
+		maxCounterPeriod = (uint16_t)nTim->Init.Period * maxSpeedPercent / 100.0;
 	}else{
-		MD[number].maxCounterPeriod = (uint16_t)MD[number].pTim->Init.Period * maxSpeedPercent / 100;
+		maxCounterPeriod = (uint16_t)pTim->Init.Period * maxSpeedPercent / 100.0;
   	}
 }
 
-void ControlMD::start(uint8_t number){
-	if(MD.find(number) == MD.end()){
-		return;
-	}
-	HAL_TIM_PWM_Start(MD[number].pTim, MD[number].pChannel);
-	HAL_TIM_PWM_Start(MD[number].nTim, MD[number].nChannel);
-	resetSpeed(number);
+void ControlMD::start(){
+	HAL_TIM_PWM_Start(pTim, pChannel);
+	HAL_TIM_PWM_Start(nTim, nChannel);
+	resetSpeed();
 }
 
-void ControlMD::stop(uint8_t number){
-	if(MD.find(number) == MD.end()){
-		return;
-	}
-	resetSpeed(number);
-	HAL_TIM_PWM_Stop(MD[number].pTim, MD[number].pChannel);
-	HAL_TIM_PWM_Stop(MD[number].nTim, MD[number].nChannel);
+void ControlMD::stop(){
+	resetSpeed();
+	HAL_TIM_PWM_Stop(pTim, pChannel);
+	HAL_TIM_PWM_Stop(nTim, nChannel);
 }
 
-void ControlMD::resetSpeed(uint8_t number){
-	if(MD.find(number) == MD.end()){
-		return;
-	}
-	__HAL_TIM_SET_COMPARE(MD[number].pTim, MD[number].pChannel, 0);
-	__HAL_TIM_SET_COMPARE(MD[number].nTim, MD[number].nChannel, 0);
+void ControlMD::resetSpeed(){
+	__HAL_TIM_SET_COMPARE(pTim, pChannel, 0);
+	__HAL_TIM_SET_COMPARE(nTim, nChannel, 0);
 }
 
-uint8_t ControlMD::setSpeed(uint8_t number, int16_t speed){
-	if(MD.find(number) == MD.end()){
-		return 1;
-	}
-	if(speed < -MD[number].maxCounterPeriod or MD[number].maxCounterPeriod < speed){
-    	resetSpeed(number);
+uint8_t ControlMD::setSpeed(int16_t speed){
+	if(speed < -maxCounterPeriod or maxCounterPeriod < speed){
+    	resetSpeed();
     	return 1;
 	}
 	if(speed < 0){
-		__HAL_TIM_SET_COMPARE(MD[number].pTim, MD[number].pChannel, 0);
-		__HAL_TIM_SET_COMPARE(MD[number].nTim, MD[number].nChannel, -speed);
+		__HAL_TIM_SET_COMPARE(pTim, pChannel, 0);
+		__HAL_TIM_SET_COMPARE(nTim, nChannel, -speed);
 	} else if(0 < speed){
-		__HAL_TIM_SET_COMPARE(MD[number].pTim, MD[number].pChannel, speed);
-		__HAL_TIM_SET_COMPARE(MD[number].nTim, MD[number].nChannel, 0);
+		__HAL_TIM_SET_COMPARE(pTim, pChannel, speed);
+		__HAL_TIM_SET_COMPARE(nTim, nChannel, 0);
 	} else {
-		__HAL_TIM_SET_COMPARE(MD[number].pTim, MD[number].pChannel, 0);
-		__HAL_TIM_SET_COMPARE(MD[number].nTim, MD[number].nChannel, 0);
+		__HAL_TIM_SET_COMPARE(pTim, pChannel, 0);
+		__HAL_TIM_SET_COMPARE(nTim, nChannel, 0);
 	}	
 	return 0;
 }
 
-void ControlMD::setSpeedPercent(uint8_t number , int8_t percent){
-	if(MD.find(number) == MD.end()){
-		return;
-	}
-	setSpeed(number, (int16_t)percent * MD[number].maxCounterPeriod * 0.01);
+void ControlMD::setSpeedPercent(int8_t percent){
+	setSpeed((int16_t)percent * MD[number].maxCounterPeriod * 0.01);
 }
--- ControlMD.hpp
+++ ControlMD.hpp
@@ -8,31 +8,26 @@
 #define CONTROL_MD_HPP_
 
 #include"tim.h"
-#include<map>
 
-typedef struct{
+class ControlMD{
+public:
+  ControlMD(){};
+  ControlMD(TIM_HandleTypeDef *pTim, uint32_t pChannel, TIM_HandleTypeDef *nTim, uint32_t nChannel)
+  :pTim(pTim),pChannel(pChannel),nTim(nTim),nChannel(nChannel){};
+  void init(float maxSpeedPercent);
+  void start();
+  void stop();
+  void resetSpeed();
+  uint8_t setSpeed(int16_t speed);
+  void setSpeedPercent(int8_t percent);
+
+private:
   TIM_HandleTypeDef *pTim;
   uint32_t pChannel;
   TIM_HandleTypeDef *nTim;
   uint32_t nChannel;
   uint16_t maxCounterPeriod;
-}MD_t;
 
-
-class ControlMD{
-public:
-  ControlMD();
-  ~ControlMD();
-  void addMD(uint8_t number, TIM_HandleTypeDef *pTim, uint32_t pChannel, TIM_HandleTypeDef *nTim, uint32_t nChannel);
-  void init(uint8_t number, double maxSpeedPercent);
-  void start(uint8_t number);
-  void stop(uint8_t number);
-  void resetSpeed(uint8_t number);
-  uint8_t setSpeed(uint8_t number, int16_t speed);
-  void setSpeedPercent(uint8_t number , int8_t percent);
-
-private:
-  std::map<uint8_t, MD_t> MD;
 };
 
 #endif /*CONTROL_MD_HPP_*/
